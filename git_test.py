f4c68b39d9d7cb4b7c4dde81f95569b2e262140a
kyl1050 <kingsleyoungl@gmail.com>
2018-12-17 15:15:08 +0800

    test2

---
+++ git_test.py
@@ -0,0 +1,45 @@
+git_test.py
+hahaahah
+aaaaaa
+
+git diff HEAD^ HEAD -- git_test.py
+
+vim git_test.py 
+git add git_test.py 
+git commit -m 'verion2'
+
+cat git_test.py 
+git_test.py
+git reset --hard HEAD^
+git reset --hard 5b46c9cb
+
+
+git reset --hard 287cbaf5936cc
+git status
+git log
+git rm -- git_test.py
+git log --pretty=oneline
+
+git checkout -b dev
+
+git branch
+
+git merge --no-ff '' dev
+
+
+git stash 
+
+git stash list
+
+github clone error :  
+eval "$(ssh-agent 0s)"
+ssh-add
+
+
+git clone "https://github.com/kyl1050/nonn.git"
+
+git push origin "分支名"   推送代码
+
+git pull origin "远程分支名"  拉取代码
+
+git branch --set-upstream-to=origin/dev dev
